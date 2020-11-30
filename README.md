a9d99856cced0267b5feec8a37236e2225477f72
abakirov93 <abakirov250593@gmail.com>
2020-11-30 00:39:24 +0000

    uploaded last changes

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # aws-vpc-iaac
 
 Here we are going to create VPC
+
+You have to create "provider.tf" file to complete this task.
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.59"
+  access_key = ""
+  secret_key = ""
+}
+
+terraform {
+  required_version = "0.11.14"
+}
---
+++ iam.tf
@@ -0,0 +1,9 @@
+# resource "aws_iam_user" "bek" {
+#   name = "bek"
+
+#   path = "/"
+# }
+
+## You can create IAM manually by going AWS account if you have an access
+## How you should create: Go to "user" then "add user" and give a user name. 
+## Choose "Select AWS access type" and go to "Set Permissions" and continue as your requirements. 
