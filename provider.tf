7968c54f34910ad5a8c1fcbac1bf802e10d83ba8
abakirov93 <63379267+abakirov93@users.noreply.github.com>
2021-03-06 11:45:59 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region  = "us-east-1"
   version = "~> 2.59"
-  access_key = "AKIA6AGASNFSCQSS5INB"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 terraform {
